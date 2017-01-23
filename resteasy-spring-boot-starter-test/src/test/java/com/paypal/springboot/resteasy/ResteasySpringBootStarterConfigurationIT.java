089395bd540ae414216f70ccad90a7409cf995db
facarvalho <facarvalho@paypal.com>
2017-01-23 12:56:01 -0600

    [13] Add automated integration tests See #13

---
+++ resteasy-spring-boot-starter-test/src/test/java/com/paypal/springboot/resteasy/ResteasySpringBootStarterConfigurationIT.java
@@ -0,0 +1,151 @@
+package com.paypal.springboot.resteasy;
+
+import com.test.Application;
+import io.restassured.response.Response;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.util.SocketUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+/**
+ * This is an integration test based on a simple sample application (see sample-app project).
+ * This class test possible configurations to register JAX-RS application classes.
+ *
+ * @author facarvalho
+ */
+public class ResteasySpringBootStarterConfigurationIT {
+
+    private int configureAndStartApp(Properties properties) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        if (properties != null) {
+            springApplication.setDefaultProperties(properties);
+        }
+
+        int port = SocketUtils.findAvailableTcpPort();
+        springApplication.run("--server.port=" + port).registerShutdownHook();
+
+        return port;
+    }
+
+    private void appShutdown(int port) {
+        Response response = given().basePath("/").port(port).post("/shutdown");
+        response.then().statusCode(200).body("message", equalTo("Shutting down, bye..."));
+    }
+
+    private void assertResourceFound(int port, String basePath) {
+        Response response = given().basePath(basePath).port(port).body("is there anybody out there?").post("/echo");
+        response.then().statusCode(200).body("timestamp", notNullValue()).body("echoText", equalTo("is there anybody out there?"));
+    }
+
+    private void assertResourceNotFound(int port, String basePath) {
+        Response response = given().basePath(basePath).port(port).body("is there anybody out there?").post("/echo");
+        response.then().statusCode(404).body("status", equalTo(404)).body("error", equalTo("Not Found"));
+    }
+
+    @Test
+    public void implicitAutoTest() {
+        int port = configureAndStartApp(null);
+
+        assertResourceFound(port, "sample-app");
+        assertResourceNotFound(port, "sample-app-test");
+        assertResourceNotFound(port, "/");
+
+        appShutdown(port);
+    }
+
+    @Test
+    public void explicitAutoTest() {
+        Properties properties = new Properties();
+        properties.put("resteasy.jaxrs.app.registration", "auto");
+
+        int port = configureAndStartApp(properties);
+
+        assertResourceFound(port, "sample-app");
+        assertResourceNotFound(port, "sample-app-test");
+        assertResourceNotFound(port, "/");
+
+        appShutdown(port);
+    }
+
+    @Test
+    public void beansTest() {
+        Properties properties = new Properties();
+        properties.put("resteasy.jaxrs.app.registration", "beans");
+
+        int port = configureAndStartApp(properties);
+
+        assertResourceFound(port, "sample-app");
+        assertResourceNotFound(port, "sample-app-test");
+        assertResourceNotFound(port, "/");
+
+        appShutdown(port);
+    }
+
+    @Test
+    public void propertySpringBeanClassTest() {
+        Properties properties = new Properties();
+        properties.put("resteasy.jaxrs.app.registration", "property");
+        properties.put("resteasy.jaxrs.app.classes", "com.test.JaxrsApplication");
+
+        int port = configureAndStartApp(properties);
+
+        assertResourceFound(port, "sample-app");
+        assertResourceNotFound(port, "sample-app-test");
+        assertResourceNotFound(port, "/");
+
+        appShutdown(port);
+    }
+
+    @Test
+    public void propertyNonSpringBeanClassTest() {
+        Properties properties = new Properties();
+        properties.put("resteasy.jaxrs.app.registration", "property");
+        properties.put("resteasy.jaxrs.app.classes", "com.test.NonSpringBeanJaxrsApplication");
+
+        int port = configureAndStartApp(properties);
+
+        assertResourceNotFound(port, "sample-app");
+        assertResourceFound(port, "sample-app-test");
+        assertResourceNotFound(port, "/");
+
+        appShutdown(port);
+    }
+
+    @Test
+    public void invalidClassTest() {
+        Properties properties = new Properties();
+        properties.put("resteasy.jaxrs.app.registration", "property");
+        properties.put("resteasy.jaxrs.app.classes", "com.foor.bar.NonExistentApplicationClass");
+
+        try {
+            configureAndStartApp(properties);
+
+            Assert.fail("Expected exception, due to class not found, has not been thrown");
+        } catch (BeansException ex) {
+            Assert.assertEquals(ex.getCause().getClass(), ClassNotFoundException.class);
+            Assert.assertEquals(ex.getCause().getMessage(), "com.foor.bar.NonExistentApplicationClass");
+        }
+    }
+
+    @Test
+    public void scanningTest() {
+        Properties properties = new Properties();
+        properties.put("resteasy.jaxrs.app.registration", "scanning");
+
+        int port = configureAndStartApp(properties);
+
+        assertResourceFound(port, "sample-app");
+        assertResourceFound(port, "sample-app-test");
+        assertResourceNotFound(port, "/");
+
+        appShutdown(port);
+    }
+
+}
---
+++ resteasy-spring-boot-starter-test/src/test/java/com/paypal/springboot/resteasy/ResteasySpringBootStarterIT.java
@@ -0,0 +1,56 @@
+package com.paypal.springboot.resteasy;
+
+import com.test.Application;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.springframework.boot.SpringApplication;
+import org.springframework.util.SocketUtils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+/**
+ * This is an integration test based on a simple sample application (see sample-app project)
+ *
+ * @author facarvalho
+ */
+public class ResteasySpringBootStarterIT {
+
+    @BeforeClass
+    public void beforeClass() {
+        RestAssured.basePath = "sample-app";
+        int port = SocketUtils.findAvailableTcpPort();
+        RestAssured.port = port;
+        SpringApplication.run(Application.class, "--server.port=" + port).registerShutdownHook();
+    }
+
+    @Test
+    public void happyPathTest() {
+        Response response = given().body("is there anybody out there?").post("/echo");
+        response.then().statusCode(200).body("timestamp", notNullValue()).body("echoText", equalTo("is there anybody out there?"));
+    }
+
+    @Test
+    public void invalidUriPathTest() {
+        // Notice "eco" is supposed to result in 404
+        Response response = given().body("is there anybody out there?").post("/eco");
+        response.then().statusCode(404).body(equalTo(""));
+    }
+
+    @Test
+    public void invalidBaseUrlTest() {
+        // Notice "sampl-ap" is supposed to result in 404
+        Response response = given().basePath("sampl-app").body("is there anybody out there?").post("/echo");
+        response.then().statusCode(404).body("status", equalTo(404)).body("error", equalTo("Not Found"));
+    }
+
+    @Test
+    public void actuatorTest() throws InterruptedException {
+        Response response = given().basePath("/").get("/health");
+        response.then().statusCode(200).body("status", equalTo("UP"));
+    }
+
+}
